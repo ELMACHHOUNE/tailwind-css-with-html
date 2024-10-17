c79e223703ed74bec87e2276cd3cdd45fb94ceb8
ELMACHHOUNE <mohamed20rida@gmail.com>
2024-10-17 17:15:50 +0100

    all project test

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+
+module.exports = {
+  content: [
+    './index.html', 
+    './contact.html', // Tell Tailwind to scan your index.html file
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
